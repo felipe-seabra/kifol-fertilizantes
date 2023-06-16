afc25901bdf8fc9a75758471b85844d7ce1ae1c2
Felipe Seabra <feliperoce@gmail.com>
2023-06-16 11:45:26 -0300

    fix: products db description

--- src/database/products.ts
+++ src/database/products.ts
@@ -6,6 +6,6 @@ export const products: IProduct[] = [
     id: 1,
     image: aminoFlower,
     name: 'AMINO FLOWER',
-    description: `O KIFOL AMINO FLOWER é um fertilizante foliar de líquido facilmente absorvido pelas folhas, caules e raízes das plantas. Sua formulação rica em zinco, boro, molibdênio, cobalto, manganês e ferro, proporcionam uma nutrição mais equilibrada, ajudando a melhorar a produtividade e a qualidade da lavoura. Seu elevado conteúdo nutricional, quando usado em épocas e recomendações apropriadas, melhora a recuperação de plantas em condições de stress. Este fertilizante melhora o aspecto visual dos frutos como: brilho e coloração. A. Flower, ainda é um estimulador de ?orada, o que proporciona um aumento de produção. O uso racional deste produto, tem como conseqüência o aumento do calibre e homogeneidade dos frutos.`
+    description: `O KIFOL AMINO FLOWER é um fertilizante foliar de líquido facilmente absorvido pelas folhas, caules e raízes das plantas. Sua formulação rica em zinco, boro, molibdênio, cobalto, manganês e ferro, proporcionam uma nutrição mais equilibrada, ajudando a melhorar a produtividade e a qualidade da lavoura. Seu elevado conteúdo nutricional, quando usado em épocas e recomendações apropriadas, melhora a recuperação de plantas em condições de stress. Este fertilizante melhora o aspecto visual dos frutos como: brilho e coloração. A Flower, ainda é um estimulador de borda, o que proporciona um aumento de produção. O uso racional deste produto, tem como conseqüência o aumento do calibre e homogeneidade dos frutos.`
   }
 ];
