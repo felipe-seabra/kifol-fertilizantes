05108646b915c0b9dd6b1e500ef14b8306c214e0
Felipe Seabra <feliperoce@gmail.com>
2023-06-16 17:22:32 -0300

    chore: inser box-shadow in image

--- src/pages/Products/styles.ts
+++ src/pages/Products/styles.ts
@@ -51,4 +51,5 @@ export const ProductImage = styled.img`
   width: 300px;
   border: 1px solid ${(props) => props.theme.colors.secundary};
   border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
 `;
