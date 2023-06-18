d9e9348882ab721a263faccbb1d0b257e4ed5bb5
Felipe Seabra <feliperoce@gmail.com>
2023-06-18 18:21:29 -0300

    chore: update texts about

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -4,8 +4,7 @@ import { Container, LogoAbout, TextContainer } from './styles';
 import { logo } from '../../images';
 
 const text = [
-  `A empresa KIFOL FERTILIZANTES está localizada em Presidente Prudente, no 
-  Oeste Paulista, e possui 45 anos de experiência no mercado, sendo uma das pioneiras no ramo.`,
+  `A empresa KIFOL FERTILIZANTES está localizada em Presidente Prudente, no Oeste Paulista, e possui mais de 45 anos de experiência no mercado, sendo uma das pioneiras no ramo.`,
 
   `Desde o início, a KIFOL FERTILIZANTES tem se destacado por sua abordagem tecnológica, buscando constantemente inovações e novas perspectivas que facilitem a vida dos agricultores. Com uma visão futurista, a empresa concentra seus esforços em trabalhar exclusivamente com fertilizantes altamente balanceados e formulações exclusivas, que atendem praticamente todas as culturas produzidas no Brasil.`
 ];
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -5,7 +5,7 @@ import { Container, LogoAbout } from './styles';
 import { logo } from '../../images';
 
 const TEXTS = [
-  `A empresa KIFOL FERTILIZANTES está localizada em Presidente Prudente, no Oeste Paulista, e possui 45 anos de experiência no mercado, sendo uma das pioneiras no ramo.`,
+  `A empresa KIFOL FERTILIZANTES está localizada em Presidente Prudente, no Oeste Paulista, e possui mais de 45 anos de experiência no mercado, sendo uma das pioneiras no ramo.`,
 
   `Desde o início, a KIFOL FERTILIZANTES tem se destacado por sua abordagem tecnológica, buscando constantemente inovações e novas perspectivas que facilitem a vida dos agricultores. Com uma visão futurista, a empresa concentra seus esforços em trabalhar exclusivamente com fertilizantes altamente balanceados e formulações exclusivas, que atendem praticamente todas as culturas produzidas no Brasil.`,
 
