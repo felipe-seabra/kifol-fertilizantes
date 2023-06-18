ff12d6b803ffed597f32b3095834add51b935d33
Felipe Seabra <feliperoce@gmail.com>
2023-06-18 18:26:33 -0300

    chore: insert texts in database

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -3,18 +3,14 @@ import React from 'react';
 import { Container, LogoAbout, TextContainer } from './styles';
 import { logo } from '../../images';
 
-const text = [
-  `A empresa KIFOL FERTILIZANTES está localizada em Presidente Prudente, no Oeste Paulista, e possui mais de 45 anos de experiência no mercado, sendo uma das pioneiras no ramo.`,
-
-  `Desde o início, a KIFOL FERTILIZANTES tem se destacado por sua abordagem tecnológica, buscando constantemente inovações e novas perspectivas que facilitem a vida dos agricultores. Com uma visão futurista, a empresa concentra seus esforços em trabalhar exclusivamente com fertilizantes altamente balanceados e formulações exclusivas, que atendem praticamente todas as culturas produzidas no Brasil.`
-];
+import { TEXTS } from '../../database/texts';
 
 function About() {
   return (
     <Container>
       <LogoAbout src={logo} alt="" />
       <TextContainer>
-        {text.map((paragraph) => (
+        {TEXTS.slice(0, 2).map((paragraph) => (
           <p>{paragraph}</p>
         ))}
       </TextContainer>
---
+++ src/database/texts.ts
@@ -0,0 +1,7 @@
+export const TEXTS = [
+  `A empresa KIFOL FERTILIZANTES está localizada em Presidente Prudente, no Oeste Paulista, e possui mais de 45 anos de experiência no mercado, sendo uma das pioneiras no ramo.`,
+
+  `Desde o início, a KIFOL FERTILIZANTES tem se destacado por sua abordagem tecnológica, buscando constantemente inovações e novas perspectivas que facilitem a vida dos agricultores. Com uma visão futurista, a empresa concentra seus esforços em trabalhar exclusivamente com fertilizantes altamente balanceados e formulações exclusivas, que atendem praticamente todas as culturas produzidas no Brasil.`,
+
+  `Ao longo dos anos, a KIFOL FERTILIZANTES tem mantido um compromisso inabalável com os agricultores, estabelecendo uma relação de confiança baseada em resultados concretos no campo. É essa parceria sólida que tem impulsionado o crescimento e a consolidação da empresa na agricultura brasileira.`
+];
