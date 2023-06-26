54ece2cc42eb243f0b72f53be9dab7531a8b714b
Felipe Seabra <feliperoce@gmail.com>
2023-06-26 09:05:10 -0300

    chore: convert images to webp

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,23 +1,24 @@
-export { default as logo } from './logo.png';
-export { default as logoK } from './logo-k.png';
-export { default as background } from './BG-BANNER.jpg';
-export { default as BANNER } from './BANNER.png';
-export { default as logoSeparate } from './separator.png';
+export { default as logo } from './logo.webp';
+export { default as logoK } from './logo-k.webp';
+
+export { default as BANNER } from './BANNER.webp';
+
+export { default as logoSeparate } from './separator.webp';
 
 // products
-export { default as ZERO1050 } from './products/00-10-50.png';
-export { default as ZERO91050 } from './products/09-10-50.png';
-export { default as AMINOFLOWER } from './products/AMINO-FLOWER.png';
-export { default as BORO10 } from './products/BORO10.png';
-export { default as CALCIO27 } from './products/CALCIO27.png';
-export { default as KIFOLMAX } from './products/KI-FOLMAX.png';
-export { default as CALIBRE } from './products/CALIBRE.png';
-export { default as COBMOL } from './products/COBMOL.png';
-export { default as CUPRATIL } from './products/CUPRATIL.png';
-export { default as FERTPOS } from './products/FERTPOS.png';
-export { default as FOSFITO } from './products/FOSFITO.png';
-export { default as GREENFOL } from './products/GREEN-FOL.png';
-export { default as ORGANICS } from './products/ORGANICS.png';
-export { default as STRIKE } from './products/STRIKE.png';
-export { default as TEKS } from './products/TEKS.png';
-export { default as ZINFOL } from './products/ZINFOL.png';
+export { default as ZERO1050 } from './products/00-10-50.webp';
+export { default as ZERO91050 } from './products/09-10-50.webp';
+export { default as AMINOFLOWER } from './products/AMINO-FLOWER.webp';
+export { default as BORO10 } from './products/BORO10.webp';
+export { default as CALCIO27 } from './products/CALCIO27.webp';
+export { default as KIFOLMAX } from './products/KI-FOLMAX.webp';
+export { default as CALIBRE } from './products/CALIBRE.webp';
+export { default as COBMOL } from './products/COBMOL.webp';
+export { default as CUPRATIL } from './products/CUPRATIL.webp';
+export { default as FERTPOS } from './products/FERTPOS.webp';
+export { default as FOSFITO } from './products/FOSFITO.webp';
+export { default as GREENFOL } from './products/GREEN-FOL.webp';
+export { default as ORGANICS } from './products/ORGANICS.webp';
+export { default as STRIKE } from './products/STRIKE.webp';
+export { default as TEKS } from './products/TEKS.webp';
+export { default as ZINFOL } from './products/ZINFOL.webp';
